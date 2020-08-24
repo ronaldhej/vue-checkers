f83be35a70722e66e930c41478f8b061f3fbb6d1
ronaldhej <ronaldhej@gmail.com>
2020-08-24 23:19:25 +0200

    first commit

---
+++ src/components/board.vue
@@ -0,0 +1,71 @@
+<template>
+<v-container class="d-none d-md-block">
+  <v-row 
+    no-gutters
+    justify="center"
+    class="pa-0 ma-0 justify-center"
+    dense
+  >
+    <v-col
+      v-for="y in 8"
+      :key="y"
+      cols="auto"
+      class="pa-0 ma-0"
+    >
+    <tile
+      v-for="x in 8"
+      style="height: 100px; width:100px"
+      :key="x"
+      :row="x"
+      :col="y"
+      cols="8"
+      sm="1"
+      class="pa-0 ma-0"
+      outlined
+    />
+    </v-col>
+  </v-row>
+</v-container>
+
+</template>
+
+
+
+<script>
+import tile from './tile';
+
+export default {
+  name: "board",
+
+  components: {
+    tile
+  },
+
+  data: function() {
+    return {
+      isBlack: true
+    }
+
+  },
+
+  methods: {
+        isEven: function(n) {
+            if (n % 2 === 0) {
+                return true
+            }
+        },
+
+        assignKey: function (index) {
+          return index + this.letters[index]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+  .isBlack {
+    color: black;
+  }
+
+</style>
---
+++ src/components/tile.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+      <v-card
+      outlined
+      class="pa-0 ma-0"
+      v-bind:style="{ 'background-color': colorTiles(row,col)}"
+      tile
+      height="100px"
+      width="100px"
+      ripple
+      >
+      row:{{row}}, column:{{col}} 
+      </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "tile",
+  props: {
+    row: Number,
+    col: Number
+  },
+
+  data() {
+    return {
+      tileColor: 'yellow'
+    }
+  },
+
+  methods: {
+    colorTiles: function(row, col) {
+      if (col % 2 === 0) {
+        if(row % 2 === 0) {
+          return 'gray'
+        }
+      } else {
+        if(row % 2 !== 0) {
+          return 'gray'
+        }        
+      }
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>

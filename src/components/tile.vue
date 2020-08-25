764f3b848d45be3d9de06c56e3f8d4979d0a6611
ronaldhej <ronaldhej@gmail.com>
2020-08-25 18:26:53 +0200

    Added pieces to the board in the correct places
    Added data to keep track of which piece a tile has

--- src/components/tile.vue
+++ src/components/tile.vue
@@ -3,34 +3,48 @@
       <v-card
       outlined
       class="pa-0 ma-0"
-      v-bind:style="{ 'background-color': colorTiles(row,col)}"
+      v-bind:style="{ 'background-color': colorTiles(row,col), 'background-image': placePieces(row, col)}"
       tile
       height="100px"
       width="100px"
       ripple
       >
-      row:{{row}}, column:{{col}} 
+        <v-img
+            height="100px"
+            width="100px"
+            v-once :src="placePieces(row)"
+        >
+        </v-img>
       </v-card>
   </v-container>
 </template>
 
 <script>
-export default {
+export default
+{
   name: "tile",
   props: {
     row: Number,
     col: Number
   },
 
-  data() {
+  data()
+  {
     return {
       // Data goes here
-      tileColor: 'white'
+      tileColor: 'white',
+      images:
+      {
+        blackPiece: require('../assets/blackpiece.png'),
+        whitePiece: require('../assets/whitepiece.png')
+      },
+      currentPiece: 'white'
     }
   },
 
   methods: {
-    colorTiles: function(row, col) {
+    colorTiles: function(row, col)
+    {
       if (col % 2 === 0) {
         if(row % 2 === 0) {
           this.tileColor = 'gray'
@@ -41,6 +55,34 @@ export default {
           this.tileColor = 'gray'
           return 'gray'
         }        
+      }
+    },
+
+    placePieces: function (row)
+    {
+      if (this.tileColor == 'gray')
+      {
+        switch (row)
+        {
+          case 1:
+            this.currentPiece = 'white'
+            return this.images.whitePiece
+          case 2:
+            this.currentPiece = 'white'
+            return this.images.whitePiece
+          case 3:
+            this.currentPiece = 'white'
+            return this.images.whitePiece
+          case 6:
+            this.currentPiece = 'black'
+            return this.images.blackPiece
+          case 7:
+            this.currentPiece = 'black'
+            return this.images.blackPiece
+          case 8:
+            this.currentPiece = 'black'
+            return this.images.blackPiece
+        }
       }
     }
   }
